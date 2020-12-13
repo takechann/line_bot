e6ad1fc315d89dc97d378a2139e062ba9b9b9f18
takechann <kurotake1130@gmail.com>
2020-12-13 23:06:52 +0900

    実行モジュール変更

--- src/main/java/com/linebot/linebot/KitchenSinkApplication.java
+++ src/main/java/com/linebot/linebot/KitchenSinkApplication.java
@@ -1,4 +1,24 @@
+/*
+ * Copyright 2016 LINE Corporation
+ *
+ * LINE Corporation licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
 package com.linebot.linebot;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 import com.linecorp.bot.model.event.Event;
 import com.linecorp.bot.model.event.MessageEvent;
@@ -12,9 +32,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @LineMessageHandler
-public class LinebotApplication {
+public class KitchenSinkApplication {
+    static Path downloadedContentDir;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        downloadedContentDir = Files.createTempDirectory("line-bot");
         final SpringApplication springApplication = new SpringApplication(LinebotApplication.class);
         springApplication.run(args);
     }
@@ -29,5 +51,4 @@ public class LinebotApplication {
     public void handleDefaultMessageEvent(Event event) {
         System.out.println("event: " + event);
     }
-
 }
